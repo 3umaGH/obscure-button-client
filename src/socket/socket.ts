de6b0930c9ba70f2381e0b58854cbd98b48634cd
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-06-14 01:25:34 +0300

    update import

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,4 +1,4 @@
 import { Socket, io } from 'socket.io-client'
-import { ServerToClientEvents, ClientToServerEvents } from '../../types/socket'
+import { ServerToClientEvents, ClientToServerEvents } from '../types/socket'
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(import.meta.env.VITE_SOCKET_URL)
