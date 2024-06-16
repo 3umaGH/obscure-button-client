8f04ac6f6eeea5ae3ad1b1c4b58f1736fa3749b6
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-06-16 17:56:40 +0300

    set api path

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,4 +1,6 @@
 import { Socket, io } from 'socket.io-client'
 import { ClientToServerEvents, ServerToClientEvents } from '../types/socket'
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(import.meta.env.VITE_SOCKET_URL)
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(import.meta.env.VITE_SOCKET_URL, {
+  path: '/api/socket.io',
+})
